1791fca6857cd5a568f285c52d02385f157b4723
ChrisGergler <ChrisGergler@live.com>
2021-12-15 15:48:45 -0500

    Updating

--- GalaxyPlayer.py
+++ GalaxyPlayer.py
@@ -1,30 +1,31 @@
 import vlc
 import os, time
+from datetime import *
 #import RPi.GPIO as GPIO
 
 #Vars
 
+#GPIO.setmode(GPIO.BCM)
 
-
-
-
-
-
-
-
-
+#GPIO.setup(17,GPIO.IN)
+#GPIO.setup(18,GPIO.IN)
 
 
 update = True #Update to false to exit
 
 def Main():
-    
+    # Setup logs
+    print(date.today())
+    with open(str(date.today())+'.txt','w') as file:
+        file.write("words")
+
+
     # Media Paths
     path = "Files:///home/pi/Videos/"
     nosubs = path+"Content-NoSubs.mp4"
     subs = path+"Content-Subtitles.mp4"
     splash = path+"StartScreen.mp4"
-    Instance = vlc.Instance()
+    Instance = vlc.Instance("-f")
     playlist = set([splash,subs,nosubs])
     url = [str(splash),str(subs),str(nosubs)] #Yes, this looks pretty redundant. Hopefully it's not.
 
@@ -39,13 +40,23 @@ def Main():
 
     tick = 0
     while update:
-        if(str(player.get_state()) == "State.NothingSpecial"):
+        state = player.get_state()
+
+        if(state == 'State.NothingSpecial'):
             player.play()
-        #if GPIO.input(17):
-        #   Media = Instance.media_new
-        tick+=1
-        print(player.get_state())
+            #print("Playing Initial")
+            tick +=1
+        elif(state == 'State.Ended'):
+            player.play()
+            tick +=1
+            #print("Repeat:")
+    
+
+
+
+
     
 Main()
+
 
 
